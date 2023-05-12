f6c8e58c2a6f08b170ff812227486f4c73d6afe3
eli <eliggold@gmail.com>
2023-05-12 17:16:14 -0400

    trial new output style

--- python/helper.py
+++ python/helper.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 import numpy as np, cv2 as cv, code
-#for obd to import properly on mac:
-import collections
-collections.MutableMapping = collections.abc.MutableMapping
-collections.Mapping = collections.abc.Mapping
-# end mac obd import req
-import obd
-
-DIM = (600,480)
-K=np.array([
-    [313.30178138822794,                0.0, 356.02518253081223],
-    [               0.0, 333.51402262357607, 290.35990191065844],
-    [               0.0,                0.0,                1.0]])
-D=np.array([
-    [0.0468345760181369],
-    [-0.0018991832207017748],
-    [0.009947859275560718],
-    [-6.907251074975234e-05]])
+sDIM = (600,480)
+#K=np.array([
+#    [313.30178138822794,                0.0, 356.02518253081223],
+#    [               0.0, 333.51402262357607, 290.35990191065844],
+#    [               0.0,                0.0,                1.0]])
+#D=np.array([
+#    [0.0468345760181369],
+#    [-0.0018991832207017748],
+#    [0.009947859275560718],
+#    [-6.907251074975234e-05]])
 # c = cv.VideoCapture(0,apiPreference = cv.CAP_V4L2)
 # c.release()
+
+DIM = (720, 576)
+K = np.array([[309.41085232860985, 0.0, 355.4094868125207], [0.0, 329.90981352161924, 292.2015284112677], [0.0, 0.0, 1.0]])
+D = np.array([[0.013301372417500422], [0.03857464918863361], [0.004117306147228716], [-0.008896442339724364]])
 sK = K * 5.0 / 6.0
 sK[2][2] = 1.0
-scaled_K = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(sK, D, DIM, np.eye(3), balance=1)
-mapSx, mapSy = cv.fisheye.initUndistortRectifyMap(K, D, np.eye(3), scaled_K, DIM, cv.CV_16SC2)
-new_K = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, (720,576), np.eye(3), balance=1)
+scaled_K = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(sK, D, sDIM, np.eye(3), balance=1)
+mapSx, mapSy = cv.fisheye.initUndistortRectifyMap(K, D, np.eye(3), scaled_K, sDIM, cv.CV_16SC2)
+new_K = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, DIM, np.eye(3), balance=1)
 mapx, mapy = cv.fisheye.initUndistortRectifyMap(K, D, np.eye(3), new_K, DIM, cv.CV_16SC2)
 
 def init():
@@ -42,21 +39,26 @@ def onScreen(f):
             buf.write(np.full(1600-f.shape[1],0x19ae,np.uint16))
 
 def onScreen2(f):
-    f = cv.remap(f, mapSx, mapSy, interpolation=cv.INTER_LINEAR)
-    f2 = cv.remap(f, mapx, mapy, interpolation=cv.INTER_LINEAR)
-    f2 = cv.cvtColor(cv.resize(f,(600,480)),cv.COLOR_BGR2BGR565)
+    f = cv.remap(f, mapx, mapy, interpolation=cv.INTER_LINEAR)
     f = cv.cvtColor(f,cv.COLOR_BGR2BGR565)
+    f1 = cv.resize(f,(960,768))
+    f2 = cv.resize(f,(1920,1536))[608:1088,320:1600]
+    f11 = f1[:480,:160]
+    f12 = f1[69:549,-160:]
     with open('/dev/fb0','rb+') as buf:
         for i in range(480):
-            buf.write(f[i])
+            buf.write(f11[i])
             buf.write(f2[i])
-            buf.write(np.full(1600-f.shape[1]-f2.shape[1],0x19ae,np.uint16))
+            buf.write(f12[i])
 
 def runDual():
     try:
         c = init()
         c.read()
-        while(c.isOpened()): onScreen2(c.read()[1])
+        while(c.isOpened()):
+            r,f = c.read()
+            if r:
+                onScreen2(f)
     finally:
         c.release()
 
