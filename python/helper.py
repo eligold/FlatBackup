cd6c84e90bf6edc3de51f93bfaaa9a0ebf5c6a8e
eli <eliggold@gmail.com>
2023-05-13 13:28:02 -0400

    hopefully no more cool modern art rave effect video output

--- python/helper.py
+++ python/helper.py
@@ -16,12 +16,8 @@ sDIM = (600,480)
 DIM = (720, 576)
 K = np.array([[309.41085232860985, 0.0, 355.4094868125207], [0.0, 329.90981352161924, 292.2015284112677], [0.0, 0.0, 1.0]])
 D = np.array([[0.013301372417500422], [0.03857464918863361], [0.004117306147228716], [-0.008896442339724364]])
-sK = K * 5.0 / 6.0
-sK[2][2] = 1.0
-scaled_K = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(sK, D, sDIM, np.eye(3), balance=1)
-mapSx, mapSy = cv.fisheye.initUndistortRectifyMap(K, D, np.eye(3), scaled_K, sDIM, cv.CV_16SC2)
 new_K = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, DIM, np.eye(3), balance=1)
-mapx, mapy = cv.fisheye.initUndistortRectifyMap(K, D, np.eye(3), new_K, DIM, cv.CV_16SC2)
+mapx, mapy = cv.fisheye.initUndistortRectifyMap(K, D, np.eye(3), new_K, DIM, cv.CV_32FC1)
 
 def init():
     c = cv.VideoCapture(0,apiPreference = cv.CAP_V4L2)
@@ -39,17 +35,19 @@ def onScreen(f):
             buf.write(np.full(1600-f.shape[1],0x19ae,np.uint16))
 
 def onScreen2(f):
-    f = cv.remap(f, mapx, mapy, interpolation=cv.INTER_LINEAR)
-    f = cv.cvtColor(f,cv.COLOR_BGR2BGR565)
-    f1 = cv.resize(f,(960,768))
-    f2 = cv.resize(f,(1920,1536))[608:1088,320:1600]
-    f11 = f1[:480,:160]
-    f12 = f1[69:549,-160:]
+    f = cv.resize(
+        cv.cvtColor(
+        cv.remap(f, mapx, mapy, interpolation=cv.INTER_LINEAR),
+        cv.COLOR_BGR2BGR565),
+        (960,768))
+    f1 = f[:480,:160]
+    f2 = f[69:549,-160:]
+    f = cv.resize(f,(1920,1536))[608:1088,320:1600]
     with open('/dev/fb0','rb+') as buf:
         for i in range(480):
-            buf.write(f11[i])
+            buf.write(f1[i])
+            buf.write(f[i])
             buf.write(f2[i])
-            buf.write(f12[i])
 
 def runDual():
     try:
