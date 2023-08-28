2f0603917865a6f0af5a60ec3a01a26fa2e42495
eli <eliggold@gmail.com>
2023-08-27 21:45:48 -0400

    latest additions might actually work

--- python/carCam_r0.4a.py
+++ python/carCam_r0.4a.py
@@ -8,7 +8,7 @@ from time import sleep
 from gpiozero import CPUTemperature as onboardTemp
 from evdev.ecodes import (ABS_MT_TRACKING_ID, ABS_MT_POSITION_X,
                           ABS_MT_POSITION_Y, EV_ABS)
-import evdev
+#import evdev
 from ELM327 import ELM327
 
 DIM = (720, 576) # video dimensions
@@ -63,34 +63,32 @@ q = Queue()
 
 # /dev/disk/by-id/ata-APPLE_SSD_TS128C_71DA5112K6IK-part1
 def start():
-    elm = ELM327()
-    camera = None
-    ec = 0
-    # dashcam = getCamera(int(os.path.realpath(
-    #         "/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._USB_CAMERA_SN0001-video-index0"
-    #     ).split("video")[-1]))
-    index = int(os.path.realpath(
-            "/dev/v4l/by-id/usb-MACROSIL_AV_TO_USB2.0-video-index0"
-        ).split("video")[-1])
-    
+    # dashcam_id_path = \
+    #     "/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._USB_CAMERA_SN0001-video-index0"
+    # dashcam = getCamera(int(os.path.realpath(dashcam_id_path).split("video")[-1]))
     # asyncio.ensure_future(touch_input(elm,camera,touch))
     # loop = asyncio.get_event_loop()
     # loop.run_forever()
+    elm = ELM327()
+    camera = None
+    ec = 0
+    usb_capture_id_path = "/dev/v4l/by-id/usb-MACROSIL_AV_TO_USB2.0-video-index0"
+    index = int(os.path.realpath(usb_capture_id_path).split("video")[-1])
     while(True):
         try:
             camera = getCamera(index)
             # /dev/input/by-id/... (or uuid?)
-            cmd = Popen("evtest /dev/input/event0",shell=True,stdout=PIPE,stderr=PIPE)
+            cmd = Popen('evtest /dev/input/event0',shell=True,stdout=PIPE,stderr=PIPE)
             t = Thread(target=enqueue_output, args=(cmd.stdout, q))
             t.daemon = True
             t.start()
-            res=run(['bash','-c','cat /sys/class/net/wlan0/operstate'],capture_output=True)
+            res=run('cat /sys/class/net/wlan0/operstate',shell=True,capture_output=True)
             volts = elm.volts()
-            if res.stdout == b'up\n' and volts < 12.1:
+            #if res.stdout == b'up\n' and volts < 12.1:
                 #close(elm,camera)
-                pass#exit(0)
+                #exit(0)
             if volts > 12.1:
-                run(['bash','-c','ip link set wlan0 down'])
+                run('ip link set wlan0 down',shell=True)
             success, img = getUndist(camera)
             with open('/dev/fb0','rb+') as buf:
                 while camera.isOpened():
@@ -103,7 +101,9 @@ def start():
                         except Empty:
                             break
                     success, img = getUndist(camera)
-                    onScreen(buf,img,elm) if success else errScreen(buf)
+                    mainImage = combinePerspective(img)
+                    sidebar = buildSidebar(elm)
+                    onScreen(buf,mainImage,sidebar) if success else errScreen(buf)
             sleep(0.19)
         except KeyboardInterrupt:
             bounce(elm,camera)
@@ -125,9 +125,9 @@ def errScreen(frame_buffer):
             "No Signal!",(500,200), font_face, scale, (0xc4,0xe4), 2, cv2.LINE_AA)
     for i in range(480):
         frame_buffer.write(image[i])
-    frame_buffer.seek(0,0)
+    frame_buffer.seek(0)
 
-def getCamera(camIndex=0,apiPreference=cv2.CAP_V4L2):
+def getCamera(camIndex:int,apiPreference=cv2.CAP_V4L2) -> cv2.VideoCapture:
     camera = cv2.VideoCapture(camIndex,apiPreference=apiPreference)
     camera.set(WIDTH,720)
     camera.set(HEIGHT,576)
@@ -137,12 +137,11 @@ def getCamera(camIndex=0,apiPreference=cv2.CAP_V4L2):
 def getUndist(c):
     success, image = c.read()
     if success:
-        image = cv2.resize(
-                    cv2.remap(image,mapx,mapy,interpolation=cv2.INTER_LANCZOS4),
-                SDIM,interpolation=cv2.INTER_LANCZOS4)[64:556]
+        undist = cv2.remap(image,mapx,mapy,interpolation=cv2.INTER_LANCZOS4)
+        image = cv2.resize(undist,SDIM,interpolation=cv2.INTER_LANCZOS4)[64:556]
     return success, image
 
-def putText(img, text="you forgot the text idiot", origin=(0,480),
+def putText(img, text="you forgot the text idiot", origin=(0,480), #bottom left
             color=(0xc5,0x9e,0x21),
             fontFace=cv2.FONT_HERSHEY_SIMPLEX,
             fontScale=1,
@@ -151,36 +150,56 @@ def putText(img, text="you forgot the text idiot", origin=(0,480),
         ):
     return cv2.putText(img,text,origin,fontFace,fontScale,color,thickness,lineType)
 
-def onScreen(frame_buffer,image,elm):
-    image_right = cv2.cvtColor(image,CVT3TO2B)
-    image_left = image_right[8:488,:220]
-    image_right = image_right[:480,-220:]
-    final_image = cv2.hconcat(image_left,image,image_right)
-    sidebar = putText(  # better battery interface to come
-                    np.full((480,120),0xc4e4,np.uint16),
-                    f"{elm.volts()}V",(38,335),
-                    color=(0xc5,0x9e,0x21),thickness=1,fontScale=0.5
-            )
-    sidebar = putText(sidebar,f"{elm.psi():.1f}",(4,38))
-    sidebar = putText(sidebar,"PSI",(7,76))
-    sidebar = putText(sidebar,f"{int(intemp.temperature)}C",(4,190))
-    image = cv2.cvtColor(
-                cv2.resize(image[213:453,220:740],FDIM,interpolation=cv2.INTER_LANCZOS4),
-            CVT3TO2B)
+def newOnScreen(frame_buffer,image,pos=(0,0)):
+    h,w,d = image.shape
+    if pos < (0,0) or d != 2:
+        raise Exception(f"negative position!\n{pos}")
+    # trim too wide
+    if w + pos[0] > 1600:
+        image = image[:,:1600-pos[0]]
+    # vertical offset
+    if pos[1] > 0:
+        frame_buffer.seek(pos[1]*2*1600)
+    for i in range(h):
+        # horizontal offset
+        if pos[0] > 0:
+            frame_buffer.seek(pos[0]*2,1)
+        frame_buffer.write(image[i])
+        # if not full width seek end of line
+        if w + pos[0] < 1600:
+            frame_buffer.seek((1600-w-pos[0])*2,1)
+    frame_buffer.seek(0)
+
+def onScreen(frame_buffer,image,sidebar):
     for i in range(480):
-        frame_buffer.write(final_image[i])
-        if i == 160 or i == 320:
-            frame_buffer.write(np.full((120),0x19ae,np.uint16))
-        elif i >= 339 and i <= 477:
-            #frame_buffer.write(np.full((120),0xae19,np.uint16))
+        frame_buffer.write(image[i])
+        if i >= 339 and i <= 477:
             for j in range(120):
                 if j < 55 or j > 85:
-                    frame_buffer.write(i*2<<8&(255-i*3/2))
+                    frame_buffer.write(i*2&(i-255))
                 else:
                     frame_buffer.write(0x00)
         else:
             frame_buffer.write(sidebar[i])
-    frame_buffer.seek(0,0)
+    frame_buffer.seek(0)
+
+def combinePerspective(image):
+    middle = cv2.resize(image[213:453,220:740],FDIM,interpolation=cv2.INTER_LANCZOS4)
+    combo = cv2.hconcat([image[8:488,:220],middle,image[:480,-220:]])
+    final_image = cv2.cvtColor(combo,CVT3TO2B)
+    return final_image
+
+def buildSidebar(elm):
+    base = np.full((480,120),0xc4e4,np.uint16)
+    line = np.full((120),0x19ae,np.uint16)
+    base[160] = line
+    base[320] = line    # TODO better battery interface to come
+    sidebar = putText(base,f"{elm.volts()}V",(38,335),
+                    color=(0xc5,0x9e,0x21),thickness=1,fontScale=0.5)
+    sidebar = putText(sidebar,f"{elm.psi():.1f}",(4,38))
+    sidebar = putText(sidebar,"PSI",(7,76))
+    sidebar = putText(sidebar,f"{int(intemp.temperature)}C",(4,190))
+    return sidebar
 
 def close(elm,camera):
     elm.close()
@@ -192,13 +211,8 @@ def bounce(elm,camera,ec=0):
     exit(ec)
 
 if __name__ == "__main__":
-    run(['sh','-c','echo 0 | sudo tee /sys/class/leds/PWR/brightness'])
+    run('echo 1 > /sys/class/leds/PWR/brightness',shell=True)
     start()
-    # try:
-    #     # run(['bash','-c','ip link set wlan0 down'])
-    #     start()
-    # finally:
-    #     pass # run(['bash','-c','ip link set wlan0 up'])
 
 ###############
 #  References #
