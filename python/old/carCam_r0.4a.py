d2251b6418926f09ce281b77cdf684b05a6f69e9
eli <eliggold@gmail.com>
2023-09-09 23:03:51 -0400

    trying to speed up overlay code

--- python/old/carCam_r0.4a.py
+++ python/old/carCam_r0.4a.py
@@ -4,22 +4,16 @@ import os, traceback, cv2, numpy as np
 from subprocess import run, Popen, PIPE
 from threading  import Thread
 from queue import Queue, Empty
-from collections import deque
 from time import sleep
-from obd import Unit
 from gpiozero import CPUTemperature as onboardTemp
-from ELM327 import ELM327
+from evdev.ecodes import (ABS_MT_TRACKING_ID, ABS_MT_POSITION_X,
+                          ABS_MT_POSITION_Y, EV_ABS)
 #import evdev
-# from evdev.ecodes import (ABS_MT_TRACKING_ID, ABS_MT_POSITION_X,
-#                           ABS_MT_POSITION_Y, EV_ABS)
+from ELM327 import ELM327
 
-IMAGE_WIDTH = 1480
-IMAGE_HEIGHT = 480
-PPPSI = 30          # pixels per PSI and negative Bar
-DIM = (720,576) # video dimensions
-SDIM = (960,768)
-FDIM = (1040,IMAGE_HEIGHT)
-
+DIM = (720, 576) # video dimensions
+SDIM = (960, 768)
+FDIM = (1040,480)
 
 COLOR_REC = 0xfa00 # 0x58
 COLOR_GOOD = 0x871a
@@ -27,11 +21,9 @@ COLOR_LOW = 0xc4e4
 COLOR_BAD = 0x8248
 COLOR_NORMAL = 0x19ae
 COLOR_LAYM = 0xbfe4
-COLOR_OVERLAY = (199,199,190)
-DOT = (255,0,0)
-SHADOW = (199,38,38)
-BLACK = (0,0,0)
-ALPHA = 0.57
+COLOR_OVERLAY = 0xad55
+COLOR_OVERLAY_MSB = 0xad6a
+COLOR_OVERLAY_LSB = 0x56b5
 
 CVT3TO2B = cv2.COLOR_BGR2BGR565
 WIDTH = cv2.CAP_PROP_FRAME_WIDTH
@@ -47,32 +39,15 @@ D = np.array([[0.013301372417500422], [0.03857464918863361], [0.0041173061472287
 # calculate camera values to upscale and undistort. TODO upscale later vs now
 new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, DIM, np.eye(3), balance=1)
 mapx, mapy = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), new_K, DIM, cv2.CV_32FC1)
-intemp = onboardTemp()
-show_graph = False
-psi_list = deque()
-queue = Queue()
+ALPHA = 0.76
 
-pos = (95,190)
-res = 50
-ofs = (3,5)
-sidebar_base = np.full((480,120),COLOR_LOW,np.uint16)
-sidebar_base = cv2.circle(sidebar_base,pos,9,(0xffff),2)
-sidebar_base = cv2.circle(sidebar_base,pos,8,(0),2)
-sidebar_base = cv2.rectangle(sidebar_base,(pos[0]-ofs[0]-2,pos[1]-ofs[1]-res-2),(pos[0]+ofs[0]+2,pos[1]-ofs[1]),(0xffff),1)
-sidebar_base = cv2.rectangle(sidebar_base,(pos[0]-ofs[0],pos[1]-ofs[1]-res),(pos[0]+ofs[0],pos[1]-ofs[1]),(0),1)
-sidebar_base = cv2.rectangle(sidebar_base,(pos[0]-ofs[0],pos[1]-ofs[1]-res),(pos[0]+ofs[0],pos[1]-ofs[1]),(0x630c),-1)
+intemp = onboardTemp()
 
 def enqueue_output(out, queue):
     for line in iter(out.readline, b''):
         queue.put(line)
     out.close()
-
-def add_pressure(pressure,deque):
-    entry = int(pressure*PPPSI)
-    while(len(deque)>=1479):
-        deque.pop()
-    deque.appendleft(entry)
-    return pressure
+q = Queue()
 
 # touch = evdev.InputDevice('/dev/input/event4')
 # psi queue, image queue
@@ -99,17 +74,17 @@ def start():
     # asyncio.ensure_future(touch_input(elm,camera,touch))
     # loop = asyncio.get_event_loop()
     # loop.run_forever()
-    global show_graph
     elm = ELM327()
     camera = None
+    ec = 0
     usb_capture_id_path = "/dev/v4l/by-id/usb-MACROSIL_AV_TO_USB2.0-video-index0"
     index = int(os.path.realpath(usb_capture_id_path).split("video")[-1])
     while(True):
         try:
             camera = getCamera(index)
-            cmd = Popen('evtest /dev/input/by-id/usb-HQEmbed_Multi-Touch-event-if00',
-                        shell=True,stdout=PIPE,stderr=PIPE)
-            t = Thread(target=enqueue_output, args=(cmd.stdout, queue))
+            # /dev/input/by-id/... (or uuid?)
+            cmd = Popen('evtest /dev/input/event0',shell=True,stdout=PIPE,stderr=PIPE)
+            t = Thread(target=enqueue_output, args=(cmd.stdout, q))
             t.daemon = True
             t.start()
             res=run('cat /sys/class/net/wlan0/operstate',shell=True,capture_output=True)
@@ -124,14 +99,10 @@ def start():
                 while camera.isOpened():
                     while(True):
                         try:  
-                            line = queue.get_nowait()
+                            line = q.get_nowait()
                             if(b'POSITION_X' in line and b'value' in line):
-                                if int(line.decode().split('value')[-1]) > IMAGE_WIDTH:
-                                    line = queue.get_nowait()
-                                    if int(line.decode().split('value')[-1]) < 240:
-                                        show_graph = not show_graph
-                                    else:
-                                        bounce(elm,camera)
+                                if int(line.decode().split('value')[-1]) > 1480:
+                                    bounce(elm,camera)
                         except Empty:
                             break
                     success, img = getUndist(camera)
@@ -141,7 +112,12 @@ def start():
             sleep(0.19)
         except KeyboardInterrupt:
             bounce(elm,camera)
-        except:
+        except Exception as e:
+            print(e)
+            ec += 1
+            if ec > 10:
+                ec = 0
+                raise e
             traceback.print_exc()
         finally:
             bounce(elm,camera,1)
@@ -183,16 +159,19 @@ def newOnScreen(frame_buffer,image,pos=(0,0)):
     h,w,d = image.shape
     if pos < (0,0) or d != 2:
         raise Exception(f"negative position!\n{pos}")
-    if w + pos[0] > 1600:       # trim too wide
+    # trim too wide
+    if w + pos[0] > 1600:
         image = image[:,:1600-pos[0]]
-        h,w,d = image.shape
-    if pos[1] > 0:              # vertical offset
+    # vertical offset
+    if pos[1] > 0:
         frame_buffer.seek(pos[1]*2*1600)
     for i in range(h):
-        if pos[0] > 0:          # horizontal offset
+        # horizontal offset
+        if pos[0] > 0:
             frame_buffer.seek(pos[0]*2,1)
         frame_buffer.write(image[i])
-        if w + pos[0] < 1600:   # if not full width seek end of line
+        # if not full width seek end of line
+        if w + pos[0] < 1600:
             frame_buffer.seek((1600-w-pos[0])*2,1)
     frame_buffer.seek(0)
 
@@ -206,61 +185,44 @@ def onScreen(frame_buffer,image,sidebar):
             frame_buffer.write(sidebar[i])
     frame_buffer.seek(0)
 
-def combinePerspective(image,inlay=None):
+def combinePerspective(image,inlay=None,overlay=True): # False):
     middle = cv2.resize(image[213:453,220:740],FDIM,interpolation=cv2.INTER_LANCZOS4) \
         if inlay is None else inlay
     combo = cv2.hconcat([image[8:488,:220],middle,image[:480,-220:]])
-    if show_graph: # prototype for boost graph
-        combo = addOverlay(combo)
+    if overlay: # prototype for boost graph
+        overlay_image = cv2.rectangle(combo,(32,13),(-32,-13),(COLOR_OVERLAY),-1)
+        overlay_image = cv2.rectangle(overlay_image,(13,32),(-13,-32),(COLOR_OVERLAY),-1)
+        overlay_image = cv2.circle(overlay_image,(32,32),19,(COLOR_OVERLAY),-1)
+        overlay_image = cv2.circle(overlay_image,(32,-32),19,(COLOR_OVERLAY),-1)
+        overlay_image = cv2.circle(overlay_image,(-32,-32),19,(COLOR_OVERLAY),-1)
+        overlay_image = cv2.circle(overlay_image,(-32,32),19,(COLOR_OVERLAY),-1)
+        cv2.addWeighted(overlay_image,ALPHA,combo,1-ALPHA,0,combo )
     final_image = cv2.cvtColor(combo,CVT3TO2B)
     return final_image
 
-# make boost graph here +12psi to -1bar (390px, 30px per unit), 1040 data pts
-def addOverlay(image):
-    h,w = image.shape[:2]
-    radius = 19
-    offset = radius * 2
-    overlay_image = image.copy()
-    graph_list = psi_list.copy()
-    overlay_image = cv2.rectangle(overlay_image,(offset,offset-radius),(w-offset,h-(offset-radius)),COLOR_OVERLAY,-1)
-    overlay_image = cv2.rectangle(overlay_image,(offset-radius,offset),(w-(offset-radius),h-offset),COLOR_OVERLAY,-1)
-    overlay_image = cv2.circle(overlay_image,(offset,offset),radius,COLOR_OVERLAY,-1)
-    overlay_image = cv2.circle(overlay_image,(offset,h-offset),radius,COLOR_OVERLAY,-1)
-    overlay_image = cv2.circle(overlay_image,(w-offset,h-offset),radius,COLOR_OVERLAY,-1)
-    overlay_image = cv2.circle(overlay_image,(w-offset,offset),radius,COLOR_OVERLAY,-1)
-    cv2.addWeighted(overlay_image,ALPHA,image,1-ALPHA,0,image)
-    image[25:455,44:46] = BLACK
-    image[405:407,25:1456] = BLACK
-    image[135:137,38:45] = BLACK
-    image = putText(image,"10",(25,133),color=BLACK,fontScale=0.38,thickness=1)
-    for x in range(IMAGE_WIDTH-len(psi_list),IMAGE_WIDTH):
-        try:
-            y = FDIM[1] - 2*PPPSI - 15 - graph_list.pop()
-            image[y:y+2,x:x+2] = DOT
-            image[y+2,x:x+3] = SHADOW
-            image[y:y+3,x+2] = SHADOW
-        except IndexError:
-            pass
-    return image
-
 def buildSidebar(elm):
-    sidebar = sidebar_base.copy()
-    sidebar = putText(sidebar,f"{elm.volts()}V",(19,133),
-                    color=COLOR_NORMAL,thickness=1,fontScale=0.5)
-    psi = add_pressure(elm.psi(),psi_list)
-    sidebar = putText(sidebar,f"{psi:.1f}",(4,57),color=COLOR_NORMAL,fontScale=1.19,thickness=3)
-    sidebar = putText(sidebar,"BAR" if psi < 0.0 else "PSI",(60,95),color=COLOR_BAD)
-    temp = int(intemp.temperature*res/100)
+    base = np.full((480,120),COLOR_LOW,np.uint16)
+    # TODO better battery interface to come
+    sidebar = putText(base,f"{elm.volts()}V",(38,133),
+                    color=(0xc5,0x9e,0x21),thickness=1,fontScale=0.5)
+    sidebar = putText(sidebar,f"{elm.psi():.1f}",(4,57),color=COLOR_NORMAL,fontScale=1.19,thickness=3)
+    sidebar = putText(sidebar,"PSI",(60,95),color=(COLOR_BAD))
+    temp = int(intemp.temperature)
     color = 0xf800 # red
     if temp < 20:
         color = 0xc55e # light blue
     elif temp < 40:
-        color = COLOR_LAYM # 'frog' green ;)
+        color = COLOR_LAYM # 'frog' green
     elif temp < 60:
         color = 0xc5ca # yellow
-    sidebar = cv2.circle(sidebar,pos,8,(color),-1)
-    sidebar = cv2.rectangle(sidebar,(pos[0]-ofs[0],pos[1]-ofs[1]-temp),
-                                    (pos[0]+ofs[0],pos[1]),(color),-1)
+    # sidebar = putText(sidebar,f"{int(intemp.temperature)}C",(4,190))
+    sidebar = cv2.circle(sidebar,(60,290),17,(0xffff),2)
+    sidebar = cv2.circle(sidebar,(60,290),15,(0),2)
+    sidebar = cv2.circle(sidebar,(60,290),15,(color),-1)
+    sidebar = cv2.rectangle(sidebar,(48,178),(72,278),(0xffff),2)
+    sidebar = cv2.rectangle(sidebar,(50,180),(70,280),(0),2)
+    sidebar = cv2.rectangle(sidebar,(50,180),(70,280),(0x630c),-1)
+    sidebar = cv2.rectangle(sidebar,(50,280-temp),(70,290),(color),-1)
     return sidebar
 
 def close(elm,camera):
@@ -273,11 +235,8 @@ def bounce(elm,camera,ec=0):
     exit(ec)
 
 if __name__ == "__main__":
-    run('echo 0 > /sys/class/leds/PWR/brightness',shell=True)
-    #try:
+    run('echo 1 > /sys/class/leds/PWR/brightness',shell=True)
     start()
-    #except:
-    #    traceback.print_exc()
 
 ###############
 #  References #
