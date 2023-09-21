f3d22cd06faff19cfa0001bc215f31d1ead283b7
eli <eliggold@gmail.com>
2023-09-20 21:10:15 -0400

    maybe works now but could have been oversimplified and needs more paring down

--- python/threadCam_pi3.py
+++ python/threadCam_pi3.py
@@ -4,6 +4,7 @@ from subprocess import run, Popen, PIPE
 from threading  import Thread
 from queue import Queue, Empty, Full
 from time import sleep
+from icecream import ic
 #import evdev
 # from evdev.ecodes import (ABS_MT_TRACKING_ID, ABS_MT_POSITION_X,
 #                           ABS_MT_POSITION_Y, EV_ABS)
@@ -34,12 +35,10 @@ D = np.array([[0.013301372417500422], [0.03857464918863361], [0.0041173061472287
 # calculate camera values to upscale and undistort. TODO upscale later vs now
 new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, DIM, np.eye(3), balance=1)
 mapx, mapy = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), new_K, DIM, cv2.CV_32FC1)
-queue = Queue()
-raw_image_queue = Queue(1)
-undistort_queue = Queue()
+touch_queue = Queue()
 output_queue = Queue()
 sidebar = np.full((IMAGE_HEIGHT,120),COLOR_LOW,np.uint16)
-for i in range(320,480):
+for i in range(480): #(320,480):
     for j in range(120):
         sidebar[i][j] = np.uint16(i*2&(i-255-j))
 no_signal_frame = cv2.putText(
@@ -56,27 +55,27 @@ def begin(): # /dev/disk/by-id/ata-APPLE_SSD_TS128C_71DA5112K6IK-part1
     #     "/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._USB_CAMERA_SN0001-video-index0"
     # dashcam = getCamera(int(os.path.realpath(dashcam_id_path).split("video")[-1]))
     try:
-        cmd = Popen('evtest /dev/input/by-id/usb-HQEmbed_Multi-Touch-event-if00',
-                    shell=True,stdout=PIPE,stderr=PIPE)
-        touch_thread = Thread(target=enqueue_output, args=(cmd.stdout, queue))
-        touch_thread.daemon = True
-        touch_thread.start()
         res=run('cat /sys/class/net/wlan0/operstate',shell=True,capture_output=True)
         if res.stdout == b'up\n':
             pass # raise KeyboardInterrupt
         else:
            run('ip link set wlan0 down',shell=True)
-        for f in [undistort_shop,view_shop,on_screen,get_image]:
+        for f in [get_image,on_screen]:
             t = Thread(target=f,name=f.__name__)
             t.daemon = True
             t.start()
             logger.info(f"started thread {f.__name__}")
+        cmd = Popen('evtest /dev/input/by-id/usb-HQEmbed_Multi-Touch-event-if00',
+                    shell=True,stdout=PIPE,stderr=PIPE)
+        touch_thread = Thread(target=enqueue_output, args=(cmd.stdout, touch_queue))
+        touch_thread.daemon = True
+        touch_thread.start()
         while(True):
             try:
-                line = queue.get_nowait()
+                line = touch_queue.get_nowait()
                 if(b'POSITION_X' in line and b'value' in line):
                     if int(line.decode().split('value')[-1]) > IMAGE_WIDTH:
-                        line = queue.get_nowait()
+                        line = touch_queue.get_nowait()
                         if int(line.decode().split('value')[-1]) < 240:
                             show_graph = not show_graph
                         else:
@@ -97,7 +96,7 @@ def get_camera(camIndex:int,apiPreference=cv2.CAP_V4L2) -> cv2.VideoCapture:
     camera.set(BRIGHTNESS,25)
     return camera
 
-def get_image(raw_image_queue=raw_image_queue):
+def get_image(output_queue=output_queue):
     usb_capture_id_path = "/dev/v4l/by-id/usb-MACROSIL_AV_TO_USB2.0-video-index0"
     usb_capture_real_path = os.path.realpath(usb_capture_id_path)
     logger.info(f"{usb_capture_id_path} -> {usb_capture_real_path}")
@@ -107,10 +106,9 @@ def get_image(raw_image_queue=raw_image_queue):
     try:
         while camera.isOpened():
             success, image = camera.read()
-            try:
-                raw_image_queue.put((success, image))
-            except Full:
-                pass
+            if success:
+                undist_image = undistort(image)
+                output_queue.put(undist_image)
     finally:
         logger.warning("release camera resource")
         camera.release()
@@ -119,43 +117,14 @@ def on_screen(output_queue=output_queue):
     with open('/dev/fb0','rb+') as frame_buffer:
         while(True):
             try:
-                image = output_queue.get_nowait()
-               # logger.info("pulled image")
-                for i in range(480):
-                    frame_buffer.write(image[i])
-                    # if i > 320:
-                    #     for j in range(120):
-                    #         frame_buffer.write(np.uint16(i*2&(i-255-j)))
-                    # else:
-                    frame_buffer.write(sidebar[i])
-                frame_buffer.seek(0)
+                image = output_queue.get()
             except Empty:
                 logger.warning("no image to display")
-
-def undistort_shop(image_queue=raw_image_queue,output_queue=undistort_queue):
-    while(True):
-        image = no_signal_frame
-        success = False
-        try:
-            success, image = image_queue.get_nowait() # timeout=0.04)
-            if success:
-                image = undistort(image)
-        except Empty:
-            logger.error("no image from camera!")
-        output_queue.put((success,image))
-
-def view_shop(image_queue=undistort_queue,output_queue=output_queue):
-    while(True):
-        image = no_signal_frame
-        success = False
-        try:
-            success, image = image_queue.get_nowait()#timeout=0.04)
-            if success:
-                image = build_reverse_view(image)
-        except Empty:
-            logging.error("no image from undistort shop")
-        output_queue.put(image)
-        
+            final_image = build_reverse_view(image)
+            for i in range(480):
+                frame_buffer.write(final_image[i])
+                frame_buffer.write(sidebar[i])
+            frame_buffer.seek(0)
 
 def undistort(img):
     undist = cv2.remap(img,mapx,mapy,interpolation=cv2.INTER_LANCZOS4)
