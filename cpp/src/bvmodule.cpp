26e56fcaf65493983db8590b109e827c44cc2d1b
eli <eliggold@gmail.com>
2024-01-28 04:37:38 -0500

    worked once and ideally also more than once

--- cpp/src/bvmodule.cpp
+++ cpp/src/bvmodule.cpp
@@ -3,7 +3,7 @@
 namespace bv {
     Size inputSize(720, 576);
     Mat undistorted, resized, middle, recolor;
-    Mat *panels[3];
+    Mat panels[3];
     const Mat cameraMatrix = (Mat_<double>(3,3) << 
             309.41085232860985, 0.0, 355.4094868125207,
             0.0, 329.90981352161924, 292.2015284112677,
@@ -13,15 +13,14 @@ namespace bv {
             0.03857464918863361,
             0.004117306147228716,
             -0.008896442339724364);
-    const Mat newCameraMatrix, undistortMapX, undistortMapY;
+    Mat newCameraMatrix, undistortMapX, undistortMapY;
     void ViewBuilder::build(InputArray image, OutputArray imview) {
         remap(image, undistorted, undistortMapX, undistortMapY, INTER_CUBIC);
         resize(undistorted.rowRange(64,556), resized, Size(960,768));
         resize(resized(Rect(220,213,520,240)).clone(), middle, Size(1040,480));
-        panels = {
-                resized(Rect(0,8,220,480)).clone(),
-                middle,
-                resized(Rect(740,0,220,480)).clone(),};
+        panels[0] = resized(Rect(0,8,220,480)).clone();
+        panels[1] = middle;
+        panels[2] = resized(Rect(740,0,220,480)).clone();
         hconcat(panels,3,recolor);
         cvtColor(recolor,imview,COLOR_BGR2BGR565);
     }
