f147b212aca7c24274d4dfdbf099e61f3dcb01e1
eli <eliggold@gmail.com>
2024-01-27 23:31:51 -0500

    nope try again cpp

--- cpp/src/bvmodule.cpp
+++ cpp/src/bvmodule.cpp
@@ -1,16 +1,28 @@
 #include"bvmodule.hpp"
 
 namespace bv {
+    Size inputSize(720, 576);
+    Mat undistorted, resized, middle, recolor;
+    const Mat cameraMatrix = (Mat_<double>(3,3) << 
+            309.41085232860985, 0.0, 355.4094868125207,
+            0.0, 329.90981352161924, 292.2015284112677,
+            0.0, 0.0, 1.0);
+    const Mat distortionCoefficients = (Mat_<double>(4,1) << 
+            0.013301372417500422,
+            0.03857464918863361,
+            0.004117306147228716,
+            -0.008896442339724364);
+    const Mat newCameraMatrix, undistortMapX, undistortMapY;
     void ViewBuilder::build(InputArray image, OutputArray imview) {
         remap(image, undistorted, undistortMapX, undistortMapY, INTER_CUBIC);
         Rect roi(0,64,719,492);
         resize(undistorted(roi).clone(), resized, Size(960,768));
         resize(resized(Rect(220,213,520,240)).clone(), middle, Size(1040,480));
-        hconcat(new Mat[] {
-                resized(Rect(0,8,219,487)),
+        hconcat(new Mat[3] {
+                resized(Rect(0,8,219,487)).clone(),
                 middle,
-                resized(Rect(740,0,959,479)),
-            },recolor);
+                resized(Rect(740,0,959,479)).clone(),
+            },3,recolor);
         cvtColor(recolor,imview,COLOR_BGR2BGR565);
     }
     ViewBuilder::ViewBuilder() {
--- cpp/src/bvmodule.hpp
+++ cpp/src/bvmodule.hpp
@@ -5,18 +5,6 @@ using namespace std;
 using namespace cv;
 
 namespace bv {
-	Size inputSize(720, 576);
-    Mat undistorted, resized, middle, recolor;
-    const Mat cameraMatrix = (Mat_<double>(3,3) << 
-            309.41085232860985, 0.0, 355.4094868125207,
-            0.0, 329.90981352161924, 292.2015284112677,
-            0.0, 0.0, 1.0);
-    const Mat distortionCoefficients = (Mat_<double>(4,1) << 
-            0.013301372417500422,
-            0.03857464918863361,
-            0.004117306147228716,
-            -0.008896442339724364);
-    const Mat newCameraMatrix, undistortMapX, undistortMapY;
     class CV_EXPORTS_W ViewBuilder {
     public:
         CV_WRAP ViewBuilder();
