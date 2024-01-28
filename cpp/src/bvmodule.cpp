ded8cae0059b228ab21d26d721415612aa35d59b
eli <eliggold@gmail.com>
2024-01-27 22:17:40 -0500

    fine i will do it myself

--- cpp/src/bvmodule.cpp
+++ cpp/src/bvmodule.cpp
@@ -1,27 +1,7 @@
 #include"bvmodule.hpp"
 
-namespace bv
-{
-    Size inputSize(720, 576);
-    Mat undistorted, resized, middle, recolor;
-    Mat cameraMatrix = (Mat_<double>(3,3) << 
-            309.41085232860985, 0.0, 355.4094868125207,
-            0.0, 329.90981352161924, 292.2015284112677,
-            0.0, 0.0, 1.0);
-    Mat distortionCoefficients = (Mat_<double>(4,1) << 
-            0.013301372417500422,
-            0.03857464918863361,
-            0.004117306147228716,
-            -0.008896442339724364);
-    Mat newCameraMatrix, undistortMapX, undistortMapY;
-    void init() {
-        fisheye::estimateNewCameraMatrixForUndistortRectify(
-            cameraMatrix, distortionCoefficients, inputSize, Matx33d::eye(),newCameraMatrix);
-        fisheye::initUndistortRectifyMap(
-            cameraMatrix, distortionCoefficients, Mat(), newCameraMatrix,
-            inputSize, CV_32FC1, undistortMapX, undistortMapY); // CV_16SC2?
-    }
-    void buildView(InputArray image, OutputArray imview) {
+namespace bv {
+    void ViewBuilder::build(InputArray image, OutputArray imview) {
         remap(image, undistorted, undistortMapX, undistortMapY, INTER_CUBIC);
         Rect roi(0,64,719,492);
         resize(undistorted(roi).clone(), resized, Size(960,768));
@@ -32,5 +12,12 @@ namespace bv
                 resized(Rect(740,0,959,479))
             },recolor);
         cvtColor(recolor,imview,COLOR_BGR2BGR565);
+    }
+    ViewBuilder::ViewBuilder() {
+        fisheye::estimateNewCameraMatrixForUndistortRectify(
+            cameraMatrix, distortionCoefficients, inputSize, Matx33d::eye(),newCameraMatrix);
+        fisheye::initUndistortRectifyMap(
+            cameraMatrix, distortionCoefficients, Mat(), newCameraMatrix,
+            inputSize, CV_32FC1, undistortMapX, undistortMapY); // CV_16SC2?
     }
 }
