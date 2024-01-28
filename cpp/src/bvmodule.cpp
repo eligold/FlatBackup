284a68f31805a01b0dbd2eee5f9caef738339147
eli <eliggold@gmail.com>
2024-01-28 04:12:49 -0500

    fixup for older cpp w bullseye

--- cpp/src/bvmodule.cpp
+++ cpp/src/bvmodule.cpp
@@ -3,6 +3,7 @@
 namespace bv {
     Size inputSize(720, 576);
     Mat undistorted, resized, middle, recolor;
+    Mat *panels[3];
     const Mat cameraMatrix = (Mat_<double>(3,3) << 
             309.41085232860985, 0.0, 355.4094868125207,
             0.0, 329.90981352161924, 292.2015284112677,
@@ -17,11 +18,11 @@ namespace bv {
         remap(image, undistorted, undistortMapX, undistortMapY, INTER_CUBIC);
         resize(undistorted.rowRange(64,556), resized, Size(960,768));
         resize(resized(Rect(220,213,520,240)).clone(), middle, Size(1040,480));
-        hconcat(new Mat[] { // needs >c++11
+        panels = {
                 resized(Rect(0,8,220,480)).clone(),
                 middle,
-                resized(Rect(740,0,220,480)).clone(),
-            },3,recolor);
+                resized(Rect(740,0,220,480)).clone(),};
+        hconcat(panels,3,recolor);
         cvtColor(recolor,imview,COLOR_BGR2BGR565);
     }
     ViewBuilder::ViewBuilder() {
