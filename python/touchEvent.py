d681c0e3856c5bc29e3b2b416409e242d02555e4
eli <eliggold@gmail.com>
2024-02-06 00:23:52 -0500

    should work now

--- python/touchEvent.py
+++ python/touchEvent.py
@@ -1,22 +1,21 @@
+### Example evtest touch input frame:
 # Event: time 1707153627.150887, type 3 (EV_ABS), code 53 (ABS_MT_POSITION_X), value 1057
 # Event: time 1707153627.150887, type 3 (EV_ABS), code 54 (ABS_MT_POSITION_Y), value 253
 # Event: time 1707153627.150887, type 3 (EV_ABS), code 0 (ABS_X), value 1057
 # Event: time 1707153627.150887, type 3 (EV_ABS), code 1 (ABS_Y), value 253
 # Event: time 1707153627.150887, type 4 (EV_MSC), code 5 (MSC_TIMESTAMP), value 0
 # Event: time 1707153627.150887, -------------- SYN_REPORT ------------
-
 class touchEvent():
     isX = False
     isY = False
     valid = True
+
     def __init__(self, line):
             try:
-                self.line = line
                 segments = line.strip().split(", ")
-                self.time = float(segments[0].split("Event: time ")[0])
+                self.time = float(segments[0].lstrip("Eventim: "))
                 frame = segments[1:]
                 if len(frame) > 1:
-                    self.line = line.strip().split("Event: time ")[0]
                     event = frame[0].lstrip("type ").rstrip(")").split(" (")
                     self.event_id = int(event[0])
                     self.event = event[1]
@@ -29,9 +28,10 @@ class touchEvent():
                     self.code = code[1]
                     self.value = int(frame[-1].lstrip("value ").rstrip())
                 else:
-                    self.line = frame[0]
+                    self.value = frame[0]
                     self.valid = False
-            except: pass
+            except: self.value = f'error for line:\n{line}'
+
     def pretty(self):
         if self.valid:
-            return self.line
+            return f'{self.event}[{self.event_id}]: {self.code}[#{self.code_id}] -> {self.value}'
