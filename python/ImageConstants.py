5516b6c632d570de4f7a69b2163d135891adb54e
eli <eliggold@gmail.com>
2024-03-13 07:00:09 -0400

    inheritance in python is fun

--- python/ImageConstants.py
+++ python/ImageConstants.py
@@ -64,48 +64,6 @@ map1, map2 = cv.convertMaps(mapx,mapy,cv.CV_16SC2) # fixed point maps run faster
 
 usb_capture_id_path="/dev/v4l/by-id/usb-MACROSIL_AV_TO_USB2.0-video-index0"
 
-### Example evtest touch input frame:
-# Event: time 1707153627.150887, type 3 (EV_ABS), code 53 (ABS_MT_POSITION_X), value 1057
-# Event: time 1707153627.150887, type 3 (EV_ABS), code 54 (ABS_MT_POSITION_Y), value 253
-# Event: time 1707153627.150887, type 3 (EV_ABS), code 0 (ABS_X), value 1057
-# Event: time 1707153627.150887, type 3 (EV_ABS), code 1 (ABS_Y), value 253
-# Event: time 1707153627.150887, type 4 (EV_MSC), code 5 (MSC_TIMESTAMP), value 0
-# Event: time 1707153627.150887, -------------- SYN_REPORT ------------
-class touchEvent():
-    isX = False
-    isY = False
-    valid = False
-
-    def __init__(self, line):
-            try:
-                self._line = line
-                segments = line.strip().split(", ")
-                self.time = float(segments[0].lstrip("Eventim: "))
-                frame = segments[1:]
-                if len(frame) > 1:
-                    event = self._sep_field(frame[0],"type")
-                    self.event_id = int(event[0])
-                    self.event = event[1]
-                    code = self._sep_field(frame[1],"code")
-                    self.code = code[1]
-                    self.code_id = int(code[0])
-                    if self.code_id == 0: self.isX = True
-                    elif self.code_id == 1: self.isY = True
-                    self.value = int(frame[-1].lstrip("value ").rstrip())
-                    self.valid = True
-                else: self.code = frame[0]
-            except:
-                traceback.print_exc()
-                self.code = f'error for line:\n{line}'
-
-    def _sep_field(self, text, key):
-        return text.lstrip(f"{key} ").rstrip(")").split(" (")
-
-    def pretty(self):
-        if self.valid:
-            return f'{self.event}[{self.event_id}]: {self.code}[#{self.code_id}] -> {self.value}'
-        return self._line
-
 def putText(img, text, origin=(0,480), #bottom left
             color=(0xc5,0x9e,0x21),fontFace=cv.FONT_HERSHEY_SIMPLEX,
             fontScale=1.19,thickness=2,lineType=cv.LINE_AA):
