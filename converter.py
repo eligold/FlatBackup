b6b0621aa875bacc6ae899cb647a8faed3c3ea85
eli <eliggold@gmail.com>
2024-06-07 14:09:34 -0400

    try multiprocessing and do away with signal queues

--- converter.py
+++ converter.py
@@ -2,12 +2,19 @@
 import os
 from subprocess import run
 
+fList = []
+
 def convertCurrentDir():
     for f in os.listdir():
         cmd = f"ffmpeg -f mjpeg -r 15 -i {f} -c:v h264 -r 15 {f[:-5]}mkv"
-        if f.endswith(".mjpeg"): run(cmd,shell=True,check=False,text=True)
+        if f.endswith(".mjpeg"): 
+            try:
+                run(cmd,shell=True,check=True,text=True)
+            except:
+                fList.append(f)
     
 
 if __name__ == "__main__":
     convertCurrentDir()
+    print(fList)
 
